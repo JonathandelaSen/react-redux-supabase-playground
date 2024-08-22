d463566e40bb5fc12f638bff699f8aff42e74af7
Jonnie <jonathandelasen@gmail.com>
2024-08-22 17:00:58 +0200

    fix: envars for vite

--- src/sections/shared/supabase/supabaseClient.ts
+++ src/sections/shared/supabase/supabaseClient.ts
@@ -1,10 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 import { type Database } from './types/database.types'
 
-//TODO: Set env vars
 const supabase = createClient<Database>(
-  process.env.REACT_SUPABASE_PROJECT_URL!,
-  process.env.REACT_SUPABASE_ANON_KEY!,
+  import.meta.env.VITE_SUPABASE_PROJECT_URL!,
+  import.meta.env.VITE_SUPABASE_ANON_KEY!,
 )
 
 export default supabase
